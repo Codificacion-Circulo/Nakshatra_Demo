bbd553397ad31afbf253a89e8413d3d54185370d
sreyom31 <sreyom31@gmail.com>
2022-02-09 00:57:46 +0530

    basic setup with navbar footer and upload image with tfjs integration
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- web-app/src/pages/uploadImage/UploadImage.js
+++ web-app/src/pages/uploadImage/UploadImage.js
@@ -1,14 +1,14 @@
 import { useState, useEffect, useRef } from "react";
 import * as tf from "@tensorflow/tfjs";
-import "./UploadImage.css"
+import "./UploadImage.css";
 
 const url = {
   model: "model.json",
 };
 
 const UploadImage = () => {
-    const [model, setModel] = useState();
-  const fileInputRef = useRef()
+  const [model, setModel] = useState();
+  const fileInputRef = useRef();
 
   const loadModel = async (url) => {
     try {
@@ -39,34 +39,36 @@ const UploadImage = () => {
       const img = new Image();
       img.src = reader.result;
       img.onload = () => {
-        const tensor = tf.browser.fromPixels(img).resizeBilinear([224, 224]).toFloat();
+        const tensor = tf.browser
+          .fromPixels(img)
+          .resizeBilinear([224, 224])
+          .toFloat();
         const offset = tf.scalar(255.0);
         const normalized = tensor.div(offset).expandDims(0);
         const predictions = model.predict(normalized);
         const values = Array.from(predictions.dataSync());
         var pIndex = tf.argMax(predictions, 1).dataSync();
-        var classNames = ["covid", "normal", "pneumonia", "tuberculosis"];  
+        var classNames = ["covid", "normal", "pneumonia", "tuberculosis"];
         alert(classNames[pIndex]);
         console.log(values);
       };
     };
-  }
+  };
 
-
-    return (
-        <div className="uploadImage__inputHolder">
-            <input
-          type="file"
-          accept="image/*"
-          capture="camera"
-          className="uploadInput"
-          ref={fileInputRef}
-        />
-        <button className="uploadImage" onClick={handleUploadImage}>
-          Upload Image
-        </button>
-        </div>
-    );
-}
+  return (
+    <div className="uploadImage__inputHolder">
+      <input
+        type="file"
+        accept="image/*"
+        capture="camera"
+        className="uploadInput"
+        ref={fileInputRef}
+      />
+      <button className="uploadImage" onClick={handleUploadImage}>
+        Upload Image
+      </button>
+    </div>
+  );
+};
 
 export default UploadImage;
