3b6b08efbb6f1cb52848c0d9268d9153371b95cc
sreyom31 <sreyom31@gmail.com>
2022-02-09 01:50:10 +0530

    alan ai integration
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,8 +1,33 @@
+import React, { Fragment, useEffect, useRef } from "react";
 import Routes from "./Routes";
+import alanBtn from "@alan-ai/alan-sdk-web";
 
 function App() {
+  const alanBtnContainer = useRef();
+  const logoEl = useRef();
+
+  useEffect(() => {
+    alanBtn({
+      key: process.env.REACT_APP_ALAN_SDK_KEY,
+      rootEl: alanBtnContainer.current,
+      onCommand: (commandData) => {
+        console.log(commandData);
+        if (commandData.command === "command-example") {
+          if (logoEl.current) {
+            logoEl.current.style.transform = "rotate(180deg)";
+          }
+        }
+      },
+    });
+  }, []);
+
   return (
-    <Routes />
+    <Fragment>
+      <Routes />
+      <div className="App">
+        <div ref={alanBtnContainer}></div>
+      </div>
+    </Fragment>
   );
 }
 
