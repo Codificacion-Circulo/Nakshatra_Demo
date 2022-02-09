a0cbc340200e948426b54fa2fde232928c18d411
sreyom31 <sreyom31@gmail.com>
2022-02-09 23:28:47 +0530

    drag and drop upload image with model success
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- web-app/src/pages/uploadImage/UploadImage.js
+++ web-app/src/pages/uploadImage/UploadImage.js
@@ -1,12 +1,39 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useCallback, useMemo } from "react";
 import * as tf from "@tensorflow/tfjs";
+import { useDropzone } from "react-dropzone";
 import "./UploadImage.css";
 
 const url = {
   model: "model.json",
 };
 
-const UploadImage = () => {
+const baseStyle = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "20px",
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: "#eeeeee",
+  borderStyle: "dashed",
+  backgroundColor: "#fafafa",
+  color: "#bdbdbd",
+  transition: "border .3s ease-in-out",
+};
+
+const activeStyle = {
+  borderColor: "#2196f3",
+};
+
+const acceptStyle = {
+  borderColor: "#00e676",
+};
+
+const rejectStyle = {
+  borderColor: "#ff1744",
+};
+
+const UploadImage = (props) => {
   const [model, setModel] = useState();
   const fileInputRef = useRef();
 
@@ -30,7 +57,8 @@ const UploadImage = () => {
   console.log(model);
 
   const handleUploadImage = () => {
-    const file = fileInputRef.current.files[0];
+    const file = files[0];
+    console.log(file);
     const reader = new FileReader();
     reader.readAsDataURL(file);
     console.log("before");
@@ -55,19 +83,62 @@ const UploadImage = () => {
     };
   };
 
+  const [files, setFiles] = useState([]);
+  const onDrop = useCallback((acceptedFiles) => {
+    setFiles(
+      acceptedFiles.map((file) =>
+        Object.assign(file, {
+          preview: URL.createObjectURL(file),
+        })
+      )
+    );
+  }, []);
+
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({
+    onDrop,
+    accept: "image/jpeg, image/png",
+  });
+
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isDragActive ? activeStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isDragActive, isDragReject, isDragAccept]
+  );
+
+  const thumbs = files.map((file) => (
+    <div key={file.name}>
+      <img src={file.preview} alt={file.name} />
+    </div>
+  ));
+
+  useEffect(
+    () => () => {
+      files.forEach((file) => URL.revokeObjectURL(file.preview));
+    },
+    [files]
+  );
+
   return (
-    <div className="uploadImage__inputHolder">
-      <input
-        type="file"
-        accept="image/*"
-        capture="camera"
-        className="uploadInput"
-        ref={fileInputRef}
-      />
+    <section>
+      <div {...getRootProps({ style })}>
+        <input {...getInputProps()} />
+        <div>Drag and drop your images here.</div>
+      </div>
+      <aside>{thumbs}</aside>
       <button className="uploadImage" onClick={handleUploadImage}>
         Upload Image
       </button>
-    </div>
+    </section>
   );
 };
 
