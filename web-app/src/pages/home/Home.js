be3b6ad2b4fd8da10a97337b67320375d7651a82
sreyom31 <sreyom31@gmail.com>
2022-02-10 23:22:52 +0530

    landing page design
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- web-app/src/pages/home/Home.js
+++ web-app/src/pages/home/Home.js
@@ -1,8 +1,9 @@
+import LandingPage from "../landingPage/LandingPage";
 import "./Home.css"
 
 const Home = () => {
     return (
-        <div>hello</div>
+        <LandingPage />
     );
 }
 
---
+++ web-app/src/pages/landingPage/LandingPage.js
@@ -0,0 +1,32 @@
+import "./LandingPage.css";
+
+// assets imports
+import landingPageBg from "../../assets/landingPageBg.png";
+
+const LandingPage = () => {
+  return (
+    <div className="landingPage__container">
+      <div className="landingPage__rowContainer row">
+        <div className="landingPage__bgContainer col-8">
+          <img src={landingPageBg} alt="Background image" />
+        </div>
+        <div className="landingPage__bgInfoContainer col-4 d-flex justify-content-center align-items-center">
+          <div className="landingPage__bgInfo d-flex flex-column  justify-content-center">
+            <div className="landingPage__smallPHeading">
+              <p>Reliabe and Accessible to all</p>
+            </div>
+            <div className="landingPage__heading">Alice Hope MD</div>
+            <div className="landingPage__subHeading">
+              <p>Online Medical Services</p>
+            </div>
+            <div className="landingPage__checkBtn">
+                <button>Check your X-ray</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
