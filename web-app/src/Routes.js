c2bd25be48df9d85495ba00c82d42dcd982ac833
aptgetinfo <deepak.2002.02.16@gmail.com>
2022-02-11 15:31:34 +0530

    web-app changes

--- web-app/src/Routes.js
+++ web-app/src/Routes.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Home from "./pages/home/Home";
 import UploadImage from "./pages/uploadImage/UploadImage";
+import Login from './pages/login/Login';
+import SignUp from './pages/login/SignUp';
 
 const Routes = () => {
     return (
@@ -13,6 +15,12 @@ const Routes = () => {
                     </Route>
                     <Route path="/upload">
                         <UploadImage />
+                    </Route>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/signup">
+                        <SignUp />
                     </Route>
                 </Switch>
             </Layout>
