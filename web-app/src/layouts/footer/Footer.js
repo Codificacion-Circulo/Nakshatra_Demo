13551055e2196c2fcf4f31ca76d940334968cee1
aptgetinfo <deepak.2002.02.16@gmail.com>
2022-02-13 03:09:36 +0530

    footer fixed

--- web-app/src/layouts/footer/Footer.js
+++ web-app/src/layouts/footer/Footer.js
@@ -5,7 +5,7 @@ import {
 } from 'mdb-react-ui-kit';
 const Footer = () => {
   return (
-    <MDBFooter className='text-center text-white' style={{ backgroundColor: '#d8efed' }}>
+    <MDBFooter className='text-center text-white container-fluid foo' style={{ backgroundColor: '#d8efed' }}>
       <div className='text-center text-dark p-3' style={{ backgroundColor: '#d8efed' }}>
         © 2022 Copyright:
         <a className='text-dark' href='/'>
